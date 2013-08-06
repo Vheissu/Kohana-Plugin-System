95f68be911d4e51a34c9c7f03e6beeae0ae55ec6
Kerstens Maxim <admin@happydemon.org>
2013-08-06 11:21:24 +0200

    Update plugins.php

--- config/plugins.php
+++ config/plugins.php
@@ -1,5 +1,5 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 return array(
-    "plugins_dir" => MODPATH . "plugins" . DIRECTORY_SEPARATOR . "plugins".DIRECTORY_SEPARATOR
+    "dir" => "plugins"
 );
